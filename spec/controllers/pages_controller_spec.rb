3020af53513a29351ea6ced1511d06d9623de52e
vagrant <vagrant@rails-starter-box>
2014-04-05 03:23:53 +0000

    slightly ahead of the tutorial

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -14,6 +14,13 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+  end  
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
   end
 
 end
